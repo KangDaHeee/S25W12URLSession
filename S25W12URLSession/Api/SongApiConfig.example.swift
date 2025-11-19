b72bb3dac96a42d4bf3380a2fb2df3ada287e073
KangDaHeee <dhk3161@gmail.com>
2025-11-19 15:51:28 +0900

    🔐API Key

---
+++ S25W12URLSession/Api/SongApiConfig.example.swift
@@ -0,0 +1,6 @@
+
+struct SongApiConfigExample {
+    static let projectURL = "https://vvggaodebaqcwatlggzy.supabase.co"
+    static let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2Z2dhb2RlYmFxY3dhdGxnZ3p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NDY5ODMsImV4cCI6MjA3NzEyMjk4M30.L1GfkWZYLP5PoaUBgLvTtE4w2KLjFeOtnRGcw9Vdpgc"
+    static let serverURL = "\(projectURL)/rest/v1/songs?apikey=\(apiKey)"
+}
